b0db9d1f3280343b5645efb7cf27a14de538b4f5
Yunan Ling <yunan.ling@ugent.be>
2024-09-08 17:43:38 +0800

    代码测试

--- vic/vic_run/src/solve_glacier.c
+++ vic/vic_run/src/solve_glacier.c
@@ -189,15 +189,19 @@ double solve_glacier(char               overstory,
     // 表面净短波
     (*NetShortSnow) = (1.0 - *AlbedoUnder) * (*ShortUnderIn);
 
-    if ((dmy->year==2003)&&(dmy->month==7)&&(dmy->day==15)) {
-        log_info("\n-------Before\nSWE %f\nSurfW %f\nPackW %f", glacier->swq, glacier->surf_temp, glacier->pack_water);
-        // log_info("\n----Band %d\nTime %d\nSnw_Mlt1 %f\nSnw_Mlt2 %f", band, dmy->dayseconds, glacier->snow_melt, snow->melt);
-        // log_info("\n----Band %d\nTime %d\nNetSW %f\nInSW %f\nAlbedo %f", band, dmy->dayseconds, (*NetShortSnow), (*ShortUnderIn), (*AlbedoUnder));
-        // log_info("\n----Before\nBand %d\nTime %d\nSWE1 %f\nSWE2 %f", band, dmy->dayseconds, glacier->swq, snow->swq);
-        // log_info("\nSnowFall %f\nRainFall %f\nTemp %f", *snowfall, *rainfall, air_temp);
-        // log_info("\nNetSW %f\nNetLW %f", *NetShortSnow, *NetLongSnow);
-        // log_info("\nMelt %f\nSnowMelt %f\nGlacierMelt %f", melt, glacier->snow_melt, glacier->glacier_melt);
+    if (glacier->surf_water < 0.) {
+        log_warn("Band%d %d-%d-%d %d", band, dmy->year, dmy->month, dmy->day, dmy->dayseconds);
     }
+
+    // if ((dmy->year==2003)&&(dmy->month==7)&&(dmy->day==15)) {
+    //     log_info("\n-------Before\nSWE %f\nSurfW %f\nPackW %f", glacier->swq, glacier->surf_temp, glacier->pack_water);
+    //     // log_info("\n----Band %d\nTime %d\nSnw_Mlt1 %f\nSnw_Mlt2 %f", band, dmy->dayseconds, glacier->snow_melt, snow->melt);
+    //     // log_info("\n----Band %d\nTime %d\nNetSW %f\nInSW %f\nAlbedo %f", band, dmy->dayseconds, (*NetShortSnow), (*ShortUnderIn), (*AlbedoUnder));
+    //     // log_info("\n----Before\nBand %d\nTime %d\nSWE1 %f\nSWE2 %f", band, dmy->dayseconds, glacier->swq, snow->swq);
+    //     // log_info("\nSnowFall %f\nRainFall %f\nTemp %f", *snowfall, *rainfall, air_temp);
+    //     // log_info("\nNetSW %f\nNetLW %f", *NetShortSnow, *NetLongSnow);
+    //     // log_info("\nMelt %f\nSnowMelt %f\nGlacierMelt %f", melt, glacier->snow_melt, glacier->glacier_melt);
+    // }
 
     // 计算
     ErrorFlag = glacier_melt((*Le), (*NetShortSnow), Tcanopy, Tgrnd, roughness, aero_resist[*UnderStory], 
