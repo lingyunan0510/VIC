d862c080bc4d83c17bcddd8e0147d22dff3491ec
Yunan Ling <yunan.ling@ugent.be>
2024-09-09 21:12:53 +0800

    代码测试

--- vic/vic_run/src/surface_fluxes.c
+++ vic/vic_run/src/surface_fluxes.c
@@ -948,6 +948,9 @@ surface_fluxes(bool                 overstory,
                     tol_over = 0;
                 }
                 if (veg_class == 17) {
+                    if ((dmy->year==2003)&&(dmy->month==7)&(dmy->day==15)) {
+                        log_info("\nBand %d Sec %d\nRain %f PPT %f", band, dmy->dayseconds, (*rainfall), (*ppt));
+                    }
                     // log_info("------T_Surf is %f", snow->surf_temp);
                     // log_info("------Melt is %f", snow->melt);
                     // log_info("UnderStory is %d", UnderStory);
