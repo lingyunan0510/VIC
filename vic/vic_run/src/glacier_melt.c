e3643ed6fcf75e7bd5ef8d299a248827ac1222cb
Yunan Ling <yunan.ling@ugent.be>
2025-02-17 22:24:23 +0800

    先不算水汽通量

--- vic/vic_run/src/glacier_melt.c
+++ vic/vic_run/src/glacier_melt.c
@@ -213,7 +213,7 @@ int glacier_melt(   double            Le,                       // 蒸发潜热 
     glacier->glacier_melt = GlacierMelt*MM_PER_M;
 
     // 物质平衡残差
-    MassBalanceError = SnowFall - AllMelt + (glacier->vapor_flux*(-1));
+    MassBalanceError = SnowFall - AllMelt;
 
     // 将流出量转化为mm
     melt[0] *= MM_PER_M;
