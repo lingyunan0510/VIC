6aa718455a5057948b80ce78841ba9ff6e7968b9
Yunan Ling <yunan.ling@ugent.be>
2025-02-20 11:39:34 +0800

    回滚物质平衡

--- vic/vic_run/src/glacier_melt.c
+++ vic/vic_run/src/glacier_melt.c
@@ -213,7 +213,7 @@ int glacier_melt(   double            Le,                       // 蒸发潜热 
     glacier->glacier_melt = GlacierMelt*MM_PER_M;
 
     // 物质平衡残差
-    MassBalanceError = SnowFall - AllMelt;
+    MassBalanceError = SnowFall;
 
     // 将流出量转化为mm
     melt[0] *= MM_PER_M;
